3e2fc90fd496298bc19a24d52e2a3f796d54d926
Lewis Cowles <lewiscowles@me.com>
2018-03-27 20:49:24 +0100

    Automatically create user-GIMP dir if not exists

--- test/gimp-plugin/install.sh
+++ test/gimp-plugin/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/bin
+mkdir -p ~/.gimp-2.8/plug-ins
 cp ../../bin/smart-object.sh ~/bin/smart-object.sh
 cp smart-object.py ~/.gimp-2.8/plug-ins/smart-object.py
 chmod +x ~/.gimp-2.8/plug-ins/smart-object.py
